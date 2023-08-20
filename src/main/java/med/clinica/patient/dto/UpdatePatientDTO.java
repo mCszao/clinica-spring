b6c1fcff0a0570248236d4e23c05a5666985833d
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-19 20:52:08 -0400

    feat: new record to update Patient on db

--- src/main/java/med/clinica/patient/dto/UpdatePatientDTO.java
+++ src/main/java/med/clinica/patient/dto/UpdatePatientDTO.java
@@ -1,2 +1,23 @@
-package med.clinica.patient.dto;public record UpdatePatientDTO() {
+package med.clinica.patient.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.clinica.address.AddressDTO;
+
+public record UpdatePatientDTO(
+        @NotNull
+        Long id,
+
+        String name,
+        @Email
+        String mail,
+        @Pattern(regexp = "\\d{11,15}")
+        String fone
+        ,
+        @Valid
+        AddressDTO address
+
+) {
 }
