2194283fcb00ee7d08c105a10268259c92a2e8d1
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-24 02:20:48 -0400

    feat: new beans to config security policy

---
+++ src/main/java/med/clinica/spring/api/infra/AuthSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package med.clinica.spring.api.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManager -> httpSecuritySessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
