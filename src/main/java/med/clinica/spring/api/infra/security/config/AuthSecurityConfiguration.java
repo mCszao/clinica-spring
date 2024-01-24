ade5ff8e842d4b3ba93acb018670ffb1d7c5e1f1
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-24 17:26:33 -0400

    refact: fixing securityFilterChain following new releases

--- src/main/java/med/clinica/spring/api/infra/security/config/AuthSecurityConfiguration.java
+++ src/main/java/med/clinica/spring/api/infra/security/config/AuthSecurityConfiguration.java
@@ -1,7 +1,11 @@
 package med.clinica.spring.api.infra.security.config;
 
+import jakarta.servlet.Filter;
+import med.clinica.spring.api.infra.security.services.FilterSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,15 +15,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class AuthSecurityConfiguration {
+
+    @Autowired
+    FilterSecurity securityFilter;
     @Bean
-    public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(httpSecuritySessionManager -> httpSecuritySessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/auth").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
+
 
     @Bean
     public AuthenticationManager getAuthManager(AuthenticationConfiguration authConfig) throws Exception{
