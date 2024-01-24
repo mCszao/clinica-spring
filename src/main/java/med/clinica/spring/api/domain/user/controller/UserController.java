3bc1c5d765fa46d03e23d1700bf99b69ef9ed43e
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-24 03:10:16 -0400

    feat: adding logic with jwt to login on application

--- src/main/java/med/clinica/spring/api/domain/user/controller/UserController.java
+++ src/main/java/med/clinica/spring/api/domain/user/controller/UserController.java
@@ -1,1 +1,35 @@
+package med.clinica.spring.api.domain.user.controller;
+
+import jakarta.validation.Valid;
+import med.clinica.spring.api.domain.user.dto.UserDTO;
+import med.clinica.spring.api.domain.user.repository.UserEntity;
+import med.clinica.spring.api.infra.security.DTO.TokenJWT;
+import med.clinica.spring.api.infra.security.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+    @PostMapping
+    public ResponseEntity signIn(@RequestBody @Valid UserDTO dto){
+        var authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(dto.username(), dto.password()));
+        var token = tokenService.generateToken((UserEntity) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenJWT(token));
+    }
+
+
+}
