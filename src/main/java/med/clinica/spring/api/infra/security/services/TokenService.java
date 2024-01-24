c5f05a5468942fddc390ce36844e945c5104b828
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-24 03:11:06 -0400

    feat: config and generate jwt token

--- src/main/java/med/clinica/spring/api/infra/security/services/TokenService.java
+++ src/main/java/med/clinica/spring/api/infra/security/services/TokenService.java
@@ -1,4 +1,34 @@
 package med.clinica.spring.api.infra.security.services;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.clinica.spring.api.domain.user.repository.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+    @Value("${jwt.secret}")
+    private String secret;
+    public String generateToken(UserEntity user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("CLINICA")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(this.generateExpireDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("JWT error generate", exception);
+        }
+    }
+
+    public Instant generateExpireDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-04:00"));
+    }
 }
