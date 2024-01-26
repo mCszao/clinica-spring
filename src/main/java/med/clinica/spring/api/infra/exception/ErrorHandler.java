39d3271ae1981255fb4b74f9d8bc640338f3e050
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-26 01:30:25 -0400

    new handle to response any catalog error

--- src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
+++ src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
@@ -1,16 +1,26 @@
 package med.clinica.spring.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleNotFound(){
+    public ResponseEntity<Object> handleNotFound(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        return new ResponseEntity<Object>(BaseErrorResponse.of(LocalDateTime.now(), ex.getClass().getName()), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
