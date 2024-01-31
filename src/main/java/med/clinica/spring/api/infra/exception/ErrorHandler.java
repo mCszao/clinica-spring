42f217fd443bd4c471fbfbffaa5f352082cdf433
Marcos Vinicius <marc.khk11@gmail.com>
2024-01-31 00:52:07 -0400

    feat: new exception handler to treats our response

--- src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
+++ src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
@@ -1,9 +1,11 @@
 package med.clinica.spring.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -21,6 +23,11 @@ public class ErrorHandler {
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
         return new ResponseEntity<Object>(BaseErrorResponse.of(LocalDateTime.now(), ex.getClass().getName()), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleInvalidArgument(MethodArgumentNotValidException ex, WebRequest request){
+        return new ResponseEntity<>(ex.getFieldErrors().stream().map(BaseErrorResponse::new).toList(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
 
