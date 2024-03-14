66a2698c56b6ecdfefed18124661c57c24f91f6f
Marcos Vinicius <marc.khk11@gmail.com>
2024-03-14 03:15:56 -0400

    feat: custom access denied

---
+++ src/main/java/med/clinica/spring/api/infra/security/services/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package med.clinica.spring.api.infra.security.services;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            System.out.println("Acesso negado disparado");
+        }
+    }
+}
