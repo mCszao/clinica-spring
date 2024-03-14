d630c6a2040cb5cd3379e424b1f7e026a4a584c3
Marcos Vinicius <marc.khk11@gmail.com>
2024-03-14 03:14:48 -0400

    feat: new exception Hanlder to access denied, and jwt mistakes

--- src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
+++ src/main/java/med/clinica/spring/api/infra/exception/ErrorHandler.java
@@ -5,11 +5,13 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
@@ -33,6 +35,16 @@ public class ErrorHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataViolationIntegrity(){
         return new ResponseEntity<>(BaseErrorResponse.of(LocalDateTime.now(), "sensitive data duplicated, please verify your form again"), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<BaseErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+        return new ResponseEntity<>(BaseErrorResponse.of(LocalDateTime.now(), "Access Denied"), new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<BaseErrorResponse> handleAuthException(AuthException ex) {
+        return new ResponseEntity<>(BaseErrorResponse.of(LocalDateTime.now(), "Erro de autenticação jwt"), new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
 }
